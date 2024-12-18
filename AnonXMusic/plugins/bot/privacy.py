7b14dcf7e53d370790edf8af0cdb527cf83a01dc
Tosu <terebina860@gmail.com>
2024-12-18 06:22:03 +0530

    Update privacy.py

--- AnonXMusic/plugins/bot/privacy.py
+++ AnonXMusic/plugins/bot/privacy.py
@@ -1,15 +1,15 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from pyrogram.enums import ParseMode
-from Anon import app
+from AnonXMusic import app
 import config
 
 TEXT = f"""
 🔒 **Privacy Policy for {app.mention} !**
 
-Your privacy is important to us. To learn more about how we collect, use, and protect your data, please review our Privacy Policy here: [Privacy Policy]({config.PRIVACY_LINK}).
+Your privacy is important to us. To learn more about how we collect, use, and protect your data, please review our Privacy Policy here: [Privacy Policy](https://telegra.ph/BOT-HUB-Music-Bots-Privacy-Policy-12-18).
 
-If you have any questions or concerns, feel free to reach out to our [support team](https://t.me/NexGenSpam).
+If you have any questions or concerns, feel free to reach out to our [support team](https://t.me/alice_x_support).
 """
 
 @app.on_message(filters.command("privacy"))
